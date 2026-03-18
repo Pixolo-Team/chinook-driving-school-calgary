86004c65f431a5c016eceaa8ea1bcab6e4942390
nagdevtaniya <nagdevtaniya@gmail.com>
2026-03-18 19:23:18 +0530

    Code Review Fixes

---
+++ src/data/TestimonialsSection.ts
@@ -0,0 +1,31 @@
+export const TestimonialSection = {
+  assets: {
+    imgShotOfAYoungBusinessmanWorkingOnAComputerInAnOffice1:
+      "/images/TestimonialsSection/YoungBusinessman.png",
+    imgProfilePhoto:
+      "/images/TestimonialsSection/ProfilePhoto.jpg",
+    imgRatingWrap:
+      "/images/TestimonialsSection/RatingWrap.svg",
+    imgGoogleLogo:
+      "/images/TestimonialsSection/GoogleLogo.svg",
+  },
+
+  testimonials: [
+    {
+      id: "custom",
+      customReview: true,
+      review:
+        "The marketing campaign they created for us was outstanding, and we saw significant engagement. However, I wish they had provided more frequent updates during the process.",
+      name: "Henry Foster",
+      role: "Project Manager",
+    },
+    {
+      id: "standard",
+      customReview: false,
+      review:
+        "The marketing campaign they created for us was outstanding, and we saw significant engagement. However, I wish they had provided more frequent updates during the process.",
+      name: "Henry Foster",
+      role: "Project Manager",
+    },
+  ],
+};
