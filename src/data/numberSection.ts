3d7120b2aea78f5a241d7c9f5b824a425a2a82d4
nagdevtaniya <nagdevtaniya@gmail.com>
2026-03-25 20:46:24 +0530

    QC

---
+++ src/data/numberSection.ts
@@ -0,0 +1,27 @@
+const numberStats = [
+  {
+    icon: "user-check-validate",
+    value: "10k+",
+    label: "Students Trained",
+  },
+  {
+    icon: "steering-wheel",
+    value: "50k+",
+    label: "Lessons Delivered",
+  },
+  {
+    icon: "smiley-very-happy",
+    value: "99%",
+    label: "Satisfaction Rate",
+  },
+  {
+    icon: "document-contract-license",
+    value: "100%",
+    label: "Licensed Coaches",
+  },
+  {
+    icon: "cars-traffic-jam",
+    value: "15+ Yrs",
+    label: "Combined Experience",
+  },
+];
