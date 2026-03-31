73c3d1a77bce0ce3c84e6a489a38842636759670
jayants1903 <jayantsinghhh89@gmail.com>
2026-03-31 15:23:32 +0530

    Fixes

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import icon from "astro-icon";
-import tailwindcss from '@tailwindcss/vite';
-import { fileURLToPath } from 'node:url';
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
 
-const srcDir = fileURLToPath(new URL('./src', import.meta.url));
+const srcDir = fileURLToPath(new URL("./src", import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    react(),
     icon({
       iconDir: "src/assets",
     }),
@@ -17,8 +19,8 @@ export default defineConfig({
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': srcDir,
+        "@": srcDir,
       },
     },
-  }
+  },
 });
