9d35de0d04dfa122d5bfe0bb445bd5b0ae4ebdba
jayants1903 <jayantsinghhh89@gmail.com>
2026-04-15 19:30:26 +0530

    Structure.php added for sheet integration

---
+++ structure.php
@@ -0,0 +1,176 @@
+<?php
+// Template Pages Structures
+$template_structures = (object) [
+  // Add keys here to add a Page Template Structure
+];
+
+$structure = (object) [
+
+  "hero" => (object)[
+    "title" => "",
+    "description" => "",
+    "images" => array("")
+  ],
+
+  "stats" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "stat_cards" => array(
+      (object)[
+        "icon" => "",
+        "stat_number" => "",
+        "stat_suffix" => "",
+        "description" => ""
+      ]
+    )
+  ],
+
+  "faq" => (object)[
+    "title" => "",
+    "description" => "",
+    "faq_cards" => array(
+      (object)[
+        "question" => "",
+        "answer" => ""
+      ]
+    )
+  ],
+
+  "features" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "description" => "",
+    "feature_cards" => array(
+      (object)[
+        "title" => "",
+        "description" => "",
+      ]
+    )
+  ],
+
+  "courses" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "description" => ""
+  ],
+
+  "instructors" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "description" => "",
+    "instructor_cards" => array(
+      (object)[
+        "name" => "",
+        "role" => "",
+        "description" => ""
+      ]
+    )
+  ],
+
+  "testimonials" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "testimonial_card" => array(
+      (object)[
+        "name" => "",
+        "title" => "",
+        "description" => "",
+        "rating" => "",
+        "image" => ""
+      ]
+    )
+  ],
+
+  "cta" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "description" => "",
+    "image" => ""
+  ],
+
+  "contact" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "description" => "",
+    "opening_hours" => array(
+      (object)[
+        "day" => "",
+        "time" => ""
+      ]
+    )
+  ],
+
+  "about_home" => (object)[
+    "title" => "",
+    "description" => ""
+  ],
+
+  "about_story" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "description" => "",
+    "story_cards" => array(
+      (object)[
+        "title" => "",
+        "description" => ""
+      ]
+    )
+  ],
+
+  "about_founder" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "paragraphs" => array(""),
+    "image" => "",
+    "name" => "",
+    "title_role" => ""
+  ],
+
+  "courses_hero" => (object)[
+    "title" => "",
+    "description" => ""
+  ],
+
+  "courses_process" => (object)[
+    "eyebrow" => "",
+    "title" => "",
+    "description" => "",
+    "course_categories" => array(
+      (object)[
+        "title" => "",
+        "steps" => array(
+          (object)[
+            "title" => "",
+            "description" => ""
+          ]
+        )
+      ]
+    )
+  ],
+
+  "contact_hero" => (object)[
+    "title" => "",
+    "description" => ""
+  ],
+
+  "enroll_hero" => (object)[
+    "title" => "",
+    "description" => ""
+  ],
+
+  "meta" => (object)[
+    "contact" => (object)[
+      "email" => "",
+      "phone_number" => "",
+      "locations" => array(
+        (object)[
+          "title" => "",
+          "address" => "",
+          "location" => "",
+          "service_area" => ""
+        ]
+      )
+    ]
+  ]
+
+];
